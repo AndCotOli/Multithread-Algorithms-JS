3ba771819137c7c953559e9cae898402fbe1e7c3
Andreu Cotanda Oliver <cotoli23@gmail.com>
2019-11-08 19:28:17 +0100

    Add prime generation

---
+++ prime_generation/generatePrimes.js
@@ -0,0 +1,16 @@
+module.exports = function generatePrimes(start, range, min, array) {
+  let isPrime = true;
+  let end = start + range;
+  for (let i = start; i < end; i++) {
+    for (let j = min; j < Math.sqrt(end); j++) {
+      if (i !== j && i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      array.push(i);
+    }
+    isPrime = true;
+  }
+};
